481c95c0b901dbe8ba66b4c64350500f3fcac904
Thomas Cheng <thomascheng81@gmail.com>
2018-06-15 01:13:47 -0400

    Remove unused import

--- src/components/NetWorthAccounts.js
+++ src/components/NetWorthAccounts.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import compose from "lodash/fp/compose";
 import filter from "lodash/fp/filter";
