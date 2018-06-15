6378b7e5ee56b80928c7335b744ac05cadddd2f0
Thomas Cheng <thomascheng81@gmail.com>
2018-06-15 01:13:05 -0400

    Start using Breakdown component

--- src/components/NetWorthAccounts.js
+++ src/components/NetWorthAccounts.js
@@ -1,39 +1,49 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
+import compose from "lodash/fp/compose";
 import filter from "lodash/fp/filter";
 import includes from "lodash/fp/includes";
+import map from "lodash/fp/map";
+import sortBy from "lodash/fp/sortBy";
+import sumBy from "lodash/fp/sumBy";
 import Section from "./Section";
-import { StrongText, SecondaryText } from "./typeComponents";
-import ListItem from "./ListItem";
-import Amount from "./Amount";
+import { StrongText } from "./typeComponents";
+import Breakdown from "./Breakdown";
 
 const GROUPS = [
   { name: "Mortgage and Assets", types: ["mortgage", "otherAsset"] },
   { name: "Investment Accounts", types: ["investmentAccount"] },
   { name: "Savings Accounts", types: ["savings"] },
   { name: "Checking Accounts and Cash", types: ["checking", "cash"] },
-  { name: "Credit Cards", types: ["creditCard"] },
-  { name: "Other", types: ["payPal"] }
+  { name: "Credit Cards and Other", types: ["creditCard", "payPal"] }
 ];
 
-const NetWorthAccounts = ({ accounts, onToggleAccount }) => {
-  return GROUPS.map(({ name, types }) => {
+const NetWorthAccounts = ({ accounts }) => {
+  const nodes = GROUPS.map(({ name, types }) => {
     const groupAccounts = filter(account => includes(account.type)(types))(
       accounts
     );
-
-    return (
-      <Section key={name}>
-        <StrongText>{name}</StrongText>
-        {groupAccounts.map(({ balance, id, name }) => (
-          <ListItem key={id} onClick={() => onToggleAccount(id)}>
-            <SecondaryText>{name}</SecondaryText>
-            <Amount amount={balance} />
-          </ListItem>
-        ))}
-      </Section>
-    );
+    return {
+      amount: sumBy("balance")(groupAccounts),
+      name,
+      id: name,
+      nodes: compose([
+        sortBy("balance"),
+        map(({ id, name, balance }) => ({
+          amount: balance,
+          id,
+          name
+        }))
+      ])(groupAccounts)
+    };
   });
+
+  return (
+    <Section>
+      <StrongText>Accounts</StrongText>
+      <Breakdown nodes={nodes} total={0} />
+    </Section>
+  );
 };
 
 NetWorthAccounts.propTypes = {
