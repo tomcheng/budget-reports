945707dcb684cc937879f9e9104c1796eccc1ae0
Thomas Cheng <thomascheng81@gmail.com>
2018-06-20 08:56:36 -0400

    Add some sliders to adjust assumptions

--- src/components/ProjectionsBody.js
+++ src/components/ProjectionsBody.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import {
   getMortgageRate,
@@ -10,61 +10,141 @@ import {
 } from "../projectionUtils";
 import Section from "./Section";
 
-const ProjectionsBody = ({ budget }) => {
-  const { rate, paymentsLeft, mortgagePayment } = getMortgageRate(budget);
+const getInitialState = budget => {
+  const {
+    paymentsLeft: remainingMortgagePayments,
+    mortgagePayment
+  } = getMortgageRate(budget);
   const returnOnInvestments = getReturnOnInvestments(budget);
   const averageContribution = getAverageContribution(budget);
   const currentInvestments = getCurrentInvestments(budget);
   const averageExpenses = getAverageExpensesWithoutMortgage(budget);
-  const projection = getProjection({
-    numMonths: 30 * 12,
+
+  return {
+    mortgagePayment,
+    remainingMortgagePayments,
     returnOnInvestments,
     averageContribution,
-    currentInvestments
-  });
-  const retirementReturns = 0.04;
-  const monthlyRetirementReturn = (1 + retirementReturns) ** (1 / 12) - 1;
-  let m = 0;
+    currentInvestments,
+    averageExpenses,
+    retirementReturns: 0.04,
+    maxAverageExpenses: Math.ceil(averageExpenses * 2 / 1000) * 1000,
+    maxAverageContribution: Math.ceil(averageContribution * 2 / 1000) * 1000
+  };
+};
 
-  while (m < projection.length) {
-    if (m <= paymentsLeft) {
-      const remainingMortgage = mortgagePayment * (paymentsLeft - m);
-      if (
-        (projection[m] - remainingMortgage) * monthlyRetirementReturn >
-        averageExpenses
-      ) {
-        break;
-      }
-    } else {
-      if (projection[m] * monthlyRetirementReturn > averageExpenses) {
-        break;
-      }
-    }
-    m += 1;
+class ProjectionsBody extends PureComponent {
+  static propTypes = {
+    budget: PropTypes.shape({
+      transactions: PropTypes.array.isRequired
+    }).isRequired
+  };
+
+  constructor(props) {
+    super();
+
+    this.state = getInitialState(props.budget);
   }
 
-  const yearsUntilRetirement = m / 12;
+  handleChange = e => {
+    this.setState({ [e.target.name]: parseFloat(e.target.value) });
+  };
 
-  return (
-    <Section>
-      <div>Mortgage Rate: {(rate * 100).toFixed(2)}</div>
-      <div>Years remaining on mortgage: {(paymentsLeft / 12).toFixed(1)}</div>
-      <div>Mortgage payment: {mortgagePayment.toFixed(2)}</div>
-      <div>Current Investments: {currentInvestments.toFixed(2)}</div>
-      <div>
-        Average return on investments: {(returnOnInvestments * 100).toFixed(2)}
-      </div>
-      <div>Average monthly contribution: {averageContribution.toFixed(2)}</div>
-      <div>Average expenses without mortgage: {averageExpenses.toFixed(2)}</div>
-      <div>Years until retirement: {yearsUntilRetirement.toFixed(1)}</div>
-    </Section>
-  );
-};
+  render() {
+    const {
+      mortgagePayment,
+      remainingMortgagePayments,
+      returnOnInvestments,
+      averageContribution,
+      currentInvestments,
+      averageExpenses,
+      retirementReturns,
+      maxAverageExpenses,
+      maxAverageContribution
+    } = this.state;
 
-ProjectionsBody.propTypes = {
-  budget: PropTypes.shape({
-    transactions: PropTypes.array.isRequired
-  }).isRequired
-};
+    const projection = getProjection({
+      numMonths: 30 * 12,
+      returnOnInvestments,
+      averageContribution,
+      currentInvestments
+    });
+    const monthlyRetirementReturn = (1 + retirementReturns) ** (1 / 12) - 1;
+    let m = 0;
+
+    while (m < projection.length) {
+      if (m <= remainingMortgagePayments) {
+        const remainingMortgage =
+          mortgagePayment * (remainingMortgagePayments - m);
+        if (
+          (projection[m] - remainingMortgage) * monthlyRetirementReturn >
+          averageExpenses
+        ) {
+          break;
+        }
+      } else {
+        if (projection[m] * monthlyRetirementReturn > averageExpenses) {
+          break;
+        }
+      }
+      m += 1;
+    }
+
+    const yearsUntilRetirement = m / 12;
+
+    return (
+      <Section>
+        <div>Current Investments: {currentInvestments.toFixed(2)}</div>
+        <div>
+          Average return on investments:{" "}
+          {(returnOnInvestments * 100).toFixed(2)}
+        </div>
+        <div>
+          <input
+            type="range"
+            value={returnOnInvestments}
+            name="returnOnInvestments"
+            onChange={this.handleChange}
+            min={0.001}
+            max={0.2}
+            step={0.001}
+          />
+        </div>
+        <div>
+          Average monthly contribution: {averageContribution.toFixed(2)}
+        </div>
+        <div>
+          <input
+            type="range"
+            value={averageContribution}
+            name="averageContribution"
+            onChange={this.handleChange}
+            min={1}
+            max={maxAverageContribution}
+          />
+        </div>
+        <div>Mortgage payment: {mortgagePayment.toFixed(2)}</div>
+        <div>
+          Years remaining on mortgage:{" "}
+          {(remainingMortgagePayments / 12).toFixed(1)}
+        </div>
+        <div>
+          Average expenses without mortgage: {averageExpenses.toFixed(2)}
+        </div>
+        <div>
+          <input
+            type="range"
+            value={averageExpenses}
+            name="averageExpenses"
+            onChange={this.handleChange}
+            min={1}
+            max={maxAverageExpenses}
+          />
+        </div>
+        <div>Years until retirement: {yearsUntilRetirement.toFixed(1)}</div>
+      </Section>
+    );
+  }
+}
 
 export default ProjectionsBody;
