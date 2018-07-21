ea01782f39b65dbea410e345046119071ddede30
Thomas Cheng <thomascheng81@gmail.com>
2018-07-21 15:33:36 -0400

    Update loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { StrongText, SecondaryText } from "./typeComponents";
 
 const Loading = () => (
-  <div>Loading...</div>
+  <div
+    style={{
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    }}
+  >
+    <StrongText>Loading&hellip;</StrongText>
+    <SecondaryText>beep, beep, boop</SecondaryText>
+  </div>
 );
 
 Loading.propTypes = {};
