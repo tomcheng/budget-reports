a7e6f9ccd041e4d168f2a3653bb9617d3f5d0ad0
Thomas Cheng <thomascheng81@gmail.com>
2018-07-23 00:17:58 -0400

    Make body components pure

--- src/components/PayeeBody.js
+++ src/components/PayeeBody.js
@@ -1,49 +1,54 @@
-import React, { Fragment } from "react";
+import React, { Fragment, PureComponent } from "react";
 import PropTypes from "prop-types";
 import propEq from "lodash/fp/propEq";
 import { getMetadataForPayee } from "../utils";
 import TopNumbers from "./TopNumbers";
 import Transactions from "./Transactions";
 
-const PayeeBody = ({ payee, budget }) => {
-  const transactions = budget.transactions.filter(propEq("payeeId", payee.id));
-  const { amount, transactions: transactionCount } = getMetadataForPayee({
-    budget,
-    payeeId: payee.id
-  });
+class PayeeBody extends PureComponent {
+  static propTypes = {
+    budget: PropTypes.shape({
+      transactions: PropTypes.arrayOf(
+        PropTypes.shape({
+          payeeId: PropTypes.string.isRequired
+        })
+      ).isRequired,
+      payeesById: PropTypes.object.isRequired
+    }).isRequired,
+    payee: PropTypes.shape({
+      id: PropTypes.string.isRequired
+    }).isRequired
+  };
 
-  return (
-    <Fragment>
-      <TopNumbers
-        numbers={[
-          { label: "total amount", value: Math.abs(amount) },
-          { label: "transactions", value: transactionCount, currency: false },
-          {
-            label: "amount/transaction",
-            value: Math.abs(amount) / transactionCount
-          }
-        ]}
-      />
-      <Transactions
-        transactions={transactions}
-        payeesById={budget.payeesById}
-      />
-    </Fragment>
-  );
-};
+  render() {
+    const { payee, budget } = this.props;
+    const transactions = budget.transactions.filter(
+      propEq("payeeId", payee.id)
+    );
+    const { amount, transactions: transactionCount } = getMetadataForPayee({
+      budget,
+      payeeId: payee.id
+    });
 
-PayeeBody.propTypes = {
-  budget: PropTypes.shape({
-    transactions: PropTypes.arrayOf(
-      PropTypes.shape({
-        payeeId: PropTypes.string.isRequired
-      })
-    ).isRequired,
-    payeesById: PropTypes.object.isRequired
-  }).isRequired,
-  payee: PropTypes.shape({
-    id: PropTypes.string.isRequired
-  }).isRequired
-};
+    return (
+      <Fragment>
+        <TopNumbers
+          numbers={[
+            { label: "total amount", value: Math.abs(amount) },
+            { label: "transactions", value: transactionCount, currency: false },
+            {
+              label: "amount/transaction",
+              value: Math.abs(amount) / transactionCount
+            }
+          ]}
+        />
+        <Transactions
+          transactions={transactions}
+          payeesById={budget.payeesById}
+        />
+      </Fragment>
+    );
+  }
+}
 
 export default PayeeBody;
--- src/components/PayeesBody.js
+++ src/components/PayeesBody.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { getProcessedPayees } from "../utils";
 import PayeeListItem from "./PayeeListItem";
 
-const PayeesBody = ({ budget, sort }) =>
-  getProcessedPayees({ budget, sort }).map(payee => (
-    <PayeeListItem
-      key={payee.id}
-      id={payee.id}
-      name={payee.name}
-      amount={payee.amount}
-      transactions={payee.transactions}
-      budgetId={budget.id}
-    />
-  ));
+class PayeesBody extends PureComponent {
+  static propTypes = {
+    budget: PropTypes.object.isRequired,
+    sort: PropTypes.oneOf(["amount", "name", "transactions"]).isRequired
+  };
 
-PayeesBody.propTypes = {
-  budget: PropTypes.object.isRequired,
-  sort: PropTypes.oneOf(["amount", "name", "transactions"]).isRequired
-};
+  render() {
+    const { budget, sort } = this.props;
+
+    return getProcessedPayees({ budget, sort }).map(payee => (
+      <PayeeListItem
+        key={payee.id}
+        id={payee.id}
+        name={payee.name}
+        amount={payee.amount}
+        transactions={payee.transactions}
+        budgetId={budget.id}
+      />
+    ));
+  }
+}
 
 export default PayeesBody;
