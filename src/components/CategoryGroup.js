7d1014fed83f4f6126171c7cbf79b0ea93fff255
Thomas Cheng <thomascheng81@gmail.com>
2018-07-25 00:27:28 -0400

    Extract CategoryGroupBody and CategoryGroupTitle components

--- src/components/CategoryGroup.js
+++ src/components/CategoryGroup.js
@@ -1,18 +1,8 @@
-import React, { Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import filter from "lodash/fp/filter";
-import find from "lodash/fp/find";
-import includes from "lodash/fp/includes";
-import map from "lodash/fp/map";
-import matchesProperty from "lodash/fp/matchesProperty";
-import sumBy from "lodash/fp/sumBy";
-import { getCategoryLink } from "../utils";
 import PageWrapper from "./PageWrapper";
-import Dropdown from "./Dropdown";
-import Icon from "./Icon";
-import TopNumbers from "./TopNumbers";
-import SpendingChart from "./SpendingChart";
-import Transactions from "./Transactions";
+import CategoryGroupBody from "./CategoryGroupBody";
+import CategoryGroupTitle from "./CategoryGroupTitle";
 
 const CategoryGroup = ({
   authorized,
@@ -30,90 +20,16 @@ const CategoryGroup = ({
     backLink
     onAuthorize={onAuthorize}
     onRequestBudget={onRequestBudget}
-    title={() => {
-      const { categoryGroups, categories: allCategories } = budget;
-
-      const categoryGroup = find(matchesProperty("id", categoryGroupId))(
-        categoryGroups
-      );
-      const categories = filter(
-        matchesProperty("categoryGroupId", categoryGroupId)
-      )(allCategories);
-
-      return (
-        <Dropdown
-          links={categories.map(category => ({
-            to: getCategoryLink({ budgetId, categoryId: category.id }),
-            label: category.name,
-            replace: true
-          }))}
-        >
-          {({ ref, triggerStyle, onClick }) => (
-            <div
-              ref={ref}
-              onClick={onClick}
-              style={{
-                ...triggerStyle,
-                alignSelf: "stretch",
-                display: "inline-flex",
-                alignItems: "center"
-              }}
-            >
-              {categoryGroup.name}
-              <div style={{ padding: "0 8px", color: "#444" }}>
-                <Icon icon="caret-down" />
-              </div>
-            </div>
-          )}
-        </Dropdown>
-      );
-    }}
-    content={() => {
-      const {
-        payeesById,
-        categories: allCategories,
-        transactions: allTransactions
-      } = budget;
-
-      const categories = filter(
-        matchesProperty("categoryGroupId", categoryGroupId)
-      )(allCategories);
-      const categoryIds = map("id")(categories);
-      const transactionsInGroup = filter(transaction =>
-        includes(transaction.categoryId)(categoryIds)
-      )(allTransactions);
-      const transactionsForMonth = filter(
-        transaction => transaction.date.slice(0, 7) === currentMonth
-      )(transactionsInGroup);
-
-      const budgeted = sumBy("budgeted")(categories);
-      const spent = -sumBy("activity")(categories);
-      const available = sumBy("balance")(categories);
-
-      return (
-        <Fragment>
-          <TopNumbers
-            numbers={[
-              { label: "budgeted", value: budgeted },
-              { label: "spent", value: spent },
-              { label: "available", value: available }
-            ]}
-          />
-          <SpendingChart
-            budgetId={budgetId}
-            currentMonth={currentMonth}
-            total={spent + available}
-            transactions={transactionsInGroup}
-          />
-          <Transactions
-            budgetId={budgetId}
-            payeesById={payeesById}
-            transactions={transactionsForMonth}
-            linkToPayee
-          />
-        </Fragment>
-      );
-    }}
+    title={() => (
+      <CategoryGroupTitle budget={budget} categoryGroupId={categoryGroupId} />
+    )}
+    content={() => (
+      <CategoryGroupBody
+        budget={budget}
+        categoryGroupId={categoryGroupId}
+        currentMonth={currentMonth}
+      />
+    )}
   />
 );
 
---
+++ src/components/CategoryGroupBody.js
@@ -0,0 +1,74 @@
+import React, { PureComponent, Fragment } from "react";
+import PropTypes from "prop-types";
+import includes from "lodash/fp/includes";
+import filter from "lodash/fp/filter";
+import matchesProperty from "lodash/fp/matchesProperty";
+import map from "lodash/fp/map";
+import sumBy from "lodash/fp/sumBy";
+import TopNumbers from "./TopNumbers";
+import SpendingChart from "./SpendingChart";
+import Transactions from "./Transactions";
+
+class CategoryGroupBody extends PureComponent {
+  static propTypes = {
+    budget: PropTypes.shape({
+      categories: PropTypes.array.isRequired,
+      id: PropTypes.string.isRequired,
+      payeesById: PropTypes.object.isRequired,
+      transactions: PropTypes.array.isRequired
+    }).isRequired,
+    categoryGroupId: PropTypes.string.isRequired,
+    currentMonth: PropTypes.string.isRequired
+  };
+
+  render() {
+    const { budget, categoryGroupId, currentMonth } = this.props;
+    const {
+      id: budgetId,
+      payeesById,
+      categories: allCategories,
+      transactions: allTransactions
+    } = budget;
+
+    const categories = filter(
+      matchesProperty("categoryGroupId", categoryGroupId)
+    )(allCategories);
+    const categoryIds = map("id")(categories);
+    const transactionsInGroup = filter(transaction =>
+      includes(transaction.categoryId)(categoryIds)
+    )(allTransactions);
+    const transactionsForMonth = filter(
+      transaction => transaction.date.slice(0, 7) === currentMonth
+    )(transactionsInGroup);
+
+    const budgeted = sumBy("budgeted")(categories);
+    const spent = -sumBy("activity")(categories);
+    const available = sumBy("balance")(categories);
+
+    return (
+      <Fragment>
+        <TopNumbers
+          numbers={[
+            { label: "budgeted", value: budgeted },
+            { label: "spent", value: spent },
+            { label: "available", value: available }
+          ]}
+        />
+        <SpendingChart
+          budgetId={budgetId}
+          currentMonth={currentMonth}
+          total={spent + available}
+          transactions={transactionsInGroup}
+        />
+        <Transactions
+          budgetId={budgetId}
+          payeesById={payeesById}
+          transactions={transactionsForMonth}
+          linkToPayee
+        />
+      </Fragment>
+    );
+  }
+}
+
+export default CategoryGroupBody;
