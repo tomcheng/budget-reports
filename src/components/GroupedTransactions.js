bfede192279abab6d965394058b864565978b409
Thomas Cheng <thomascheng81@gmail.com>
2018-07-30 01:41:57 -0400

    Extract GroupedTransactions component

---
+++ src/components/GroupedTransactions.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import groupBy from "lodash/fp/groupBy";
+import identity from "lodash/fp/identity";
+import map from "lodash/fp/map";
+import prop from "lodash/fp/prop";
+import sumBy from "lodash/fp/sumBy";
+import Breakdown from "./Breakdown";
+
+const mapWithKeys = map.convert({ cap: false });
+
+const GroupedTransactions = ({
+  transactions,
+  groupBy: groupByFunction,
+  groupDisplayFunction,
+  leafDisplayFunction
+}) => {
+  const groupedTransactions = groupBy(groupByFunction)(transactions);
+  const nodes = mapWithKeys((transactions, key) => ({
+    amount: sumBy("amount")(transactions),
+    id: key,
+    name: (
+      <span>
+        {groupDisplayFunction(key)}{" "}
+        <span style={{ opacity: 0.6 }}>
+          &ndash; {transactions.length} transaction{transactions.length === 1
+            ? ""
+            : "s"}
+        </span>
+      </span>
+    ),
+    nodes: transactions.map(transaction => ({
+      amount: transaction.amount,
+      name: leafDisplayFunction(transaction),
+      id: transaction.id
+    }))
+  }))(groupedTransactions);
+
+  return <Breakdown nodes={nodes} />;
+};
+
+GroupedTransactions.propTypes = {
+  groupBy: PropTypes.func.isRequired,
+  transactions: PropTypes.array.isRequired,
+  groupDisplayFunction: PropTypes.func,
+  leafDisplayFunction: PropTypes.func
+};
+
+GroupedTransactions.defaultProps = {
+  groupDisplayFunction: identity,
+  leafDisplayFunction: prop("id")
+};
+
+export default GroupedTransactions;
--- src/components/PayeeBody.js
+++ src/components/PayeeBody.js
@@ -2,18 +2,14 @@ import React, { Fragment, PureComponent } from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
 import compose from "lodash/fp/compose";
-import groupBy from "lodash/fp/groupBy";
 import map from "lodash/fp/map";
 import prop from "lodash/fp/prop";
-import sumBy from "lodash/fp/sumBy";
 import uniq from "lodash/fp/uniq";
 import { getMetadataForPayee, getTransactionMonth } from "../utils";
 import TopNumbers from "./TopNumbers";
 import PayeeCategories from "./PayeeCategories";
-import Breakdown from "./Breakdown";
+import GroupedTransactions from "./GroupedTransactions";
 import Section from "./Section";
-
-const mapWithKeys = map.convert({ cap: false });
 
 class PayeeBody extends PureComponent {
   static propTypes = {
@@ -36,26 +32,6 @@ class PayeeBody extends PureComponent {
       budget,
       payeeId: payee.id
     });
-    const transactionsByMonth = groupBy(getTransactionMonth)(transactions);
-    const nodes = mapWithKeys((transactions, month) => ({
-      amount: sumBy("amount")(transactions),
-      id: month,
-      name: (
-        <span>
-          {moment(month).format("MMMM YYYY")}{" "}
-          <span style={{ opacity: 0.6 }}>
-            &ndash; {transactions.length} transaction{transactions.length === 1
-              ? ""
-              : "s"}
-          </span>
-        </span>
-      ),
-      nodes: transactions.map(transaction => ({
-        amount: transaction.amount,
-        name: moment(transaction.date).format("dddd, MMMM D"),
-        id: transaction.id
-      }))
-    }))(transactionsByMonth);
     const categoryIds = compose([uniq, map(prop("categoryId"))])(transactions);
 
     return (
@@ -80,7 +56,14 @@ class PayeeBody extends PureComponent {
           <PayeeCategories budget={budget} categoryIds={categoryIds} />
         </Section>
         <Section title="Transactions">
-          <Breakdown nodes={nodes} />
+          <GroupedTransactions
+            transactions={transactions}
+            groupBy={getTransactionMonth}
+            groupDisplayFunction={month => moment(month).format("MMMM YYYY")}
+            leafDisplayFunction={transaction =>
+              moment(transaction.date).format("dddd, MMMM D")
+            }
+          />
         </Section>
       </Fragment>
     );
