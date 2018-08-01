64b91b21ac6ccadf14869638909e9f8941b1a32c
Thomas Cheng <thomascheng81@gmail.com>
2018-08-01 01:44:08 -0400

    Start styling modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,83 @@
-import React from "react";
+import React, { Component } from "react";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const Modal = ({ open, children, onClose }) =>
-  open && (
-    <div>
-      <div onClick={onClose}>close</div>
-      {children}
-    </div>
-  );
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-Modal.propTypes = {
-  children: PropTypes.node.isRequired,
-  open: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired
-};
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div`
+  position: relative;
+  padding: 15px 20px;
+  background-color: #fff;
+  border-radius: 2px;
+  z-index: 1000;
+`;
+
+const CloseButton = styled.div`
+  position: absolute;
+  font-size: 20px;
+  line-height: 16px;
+  top: 0;
+  right: 3px;
+  padding: 5px;
+  cursor: pointer;
+`;
+
+class Modal extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    open: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired
+  };
+
+  constructor() {
+    super();
+    this.el = document.createElement("div");
+  }
+
+  componentDidMount() {
+    document.body.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.el);
+  }
+
+  render() {
+    const { onClose, open, children } = this.props;
+
+    return createPortal(
+      open && (
+        <Container>
+          <Overlay />
+          <ModalContent>
+            <CloseButton onClick={onClose}>&times;</CloseButton>
+            {children}
+          </ModalContent>
+        </Container>
+      ),
+      this.el
+    );
+  }
+}
 
 export default Modal;
