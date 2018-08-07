80e458f4f117f6afc6abac40747caf10b5f493fd
Thomas Cheng <thomascheng81@gmail.com>
2018-08-07 00:41:48 -0400

    Remove CategoryPayee page

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,7 @@
 import React, { Fragment, PureComponent } from "react";
 import PropTypes from "prop-types";
-import { getFirstMonth, getTransactionMonth } from "../utils";
-import pages, { makeLink } from "../pages";
+import { getFirstMonth } from "../utils";
 import MonthByMonthSection from "./MonthByMonthSection";
-import GenericEntitiesSection from "./GenericEntitiesSection";
 import TransactionsByMonthSection from "./TransactionsByMonthSection";
 
 class Category extends PureComponent {
@@ -25,16 +23,11 @@ class Category extends PureComponent {
 
   render() {
     const { category, budget, selectedMonth, onSelectMonth } = this.props;
-    const { transactions, payeesById, id: budgetId } = budget;
+    const { transactions, payeesById } = budget;
     const firstMonth = getFirstMonth(budget);
     const transactionsForCategory = transactions.filter(
       transaction => transaction.categoryId === category.id
     );
-    const transactionsForMonth =
-      selectedMonth &&
-      transactionsForCategory.filter(
-        transaction => getTransactionMonth(transaction) === selectedMonth
-      );
 
     return (
       <Fragment>
@@ -43,21 +36,6 @@ class Category extends PureComponent {
           transactions={transactionsForCategory}
           selectedMonth={selectedMonth}
           onSelectMonth={onSelectMonth}
-        />
-        <GenericEntitiesSection
-          entitiesById={payeesById}
-          entityKey="payeeId"
-          linkFunction={payeeId =>
-            makeLink(pages.categoryPayee.path, {
-              budgetId,
-              categoryGroupId: category.categoryGroupId,
-              categoryId: category.id,
-              payeeId
-            })
-          }
-          title="Payees"
-          transactions={transactionsForMonth || transactionsForCategory}
-          showTransactionCount
         />
         <TransactionsByMonthSection
           onSelectMonth={onSelectMonth}
