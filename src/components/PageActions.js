8825a689c9d13f02e70a0c8961ee832db7f61440
Thomas Cheng <thomascheng81@gmail.com>
2018-08-16 17:44:27 -0400

    Link current month to trends and vice versa

--- src/components/PageActions.js
+++ src/components/PageActions.js
@@ -1,11 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
-import pages from "../pages";
-import { Switch, Route } from "react-router-dom";
+import pages, { makeLink } from "../pages";
+import { Switch, Route, Link } from "react-router-dom";
 import { SecondaryText } from "./typeComponents";
 
 const PageActions = ({ settings, onChangeSetting }) => (
   <Switch>
+    <Route
+      path={pages.currentMonthGroup.path}
+      exact
+      render={({ match }) => (
+        <SecondaryText>
+          <Link to={makeLink(pages.group.path, match.params)}>Trends</Link>
+        </SecondaryText>
+      )}
+    />
+    <Route
+      path={pages.currentMonthCategory.path}
+      exact
+      render={({ match }) => (
+        <SecondaryText>
+          <Link to={makeLink(pages.category.path, match.params)}>Trends</Link>
+        </SecondaryText>
+      )}
+    />
+    <Route
+      path={pages.group.path}
+      exact
+      render={({ match }) => (
+        <SecondaryText>
+          <Link to={makeLink(pages.currentMonthGroup.path, match.params)}>
+            Current Month
+          </Link>
+        </SecondaryText>
+      )}
+    />
+    <Route
+      path={pages.category.path}
+      exact
+      render={({ match }) => (
+        <SecondaryText>
+          <Link to={makeLink(pages.currentMonthCategory.path, match.params)}>
+            Current Month
+          </Link>
+        </SecondaryText>
+      )}
+    />
     <Route
       path={pages.incomeVsExpenses.path}
       exact
