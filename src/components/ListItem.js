e27d033c45aa7786f1a487ab695e2fec6f39102e
Thomas Cheng <thomascheng81@gmail.com>
2018-08-16 23:50:16 -0400

    Remove unused components

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,4 @@
-import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 const ListItem = styled.div`
   display: flex;
@@ -13,40 +11,6 @@ const ListItem = styled.div`
   &:first-child {
     border-top: ${props => !props.isContinuing && 0};
     padding-top: ${props => !props.isContinuing && 0};
-  }
-`;
-
-const StyledLink = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 8px 0;
-  border-top: 1px dotted #ddd;
-
-  &:first-child {
-    border-top: ${props => !props.isContinuing && 0};
-    padding-top: ${props => !props.isContinuing && 0};
-  }
-`;
-
-export const ListItemLink = ({ isContinuing, ...other }) => (
-  <StyledLink
-    {...other}
-    style={isContinuing && { paddingTop: 8, borderTop: "1px dotted #ddd" }}
-  />
-);
-
-export const LargeListItemLink = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 60px;
-  padding: 0 20px;
-  border-top: 1px solid #eee;
-  color: inherit;
-
-  &:first-child {
-    border-top: 0;
   }
 `;
 
