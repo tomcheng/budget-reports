2fa1a417e948b30bbc277fdfa0f9e30e62a8a48b
Thomas Cheng <thomascheng81@gmail.com>
2018-08-19 13:15:45 -0400

    Add privacy policy to Unauthorized page

--- src/components/Unauthorized.js
+++ src/components/Unauthorized.js
@@ -1,16 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Section from "./Section";
-import { PageTitle } from "./typeComponents";
+import { PageTitle, StrongText, SecondaryText } from "./typeComponents";
 import { PrimaryButton } from "./Button";
 
 const Unauthorized = ({ onAuthorize }) => (
   <Section>
     <PageTitle>Budget Reports</PageTitle>
-    <div style={{ marginBottom: 15 }}>Get various insights from your YNAB account.</div>
+    <div style={{ marginBottom: 10 }}>Get various insights from your YNAB account.</div>
     <PrimaryButton onClick={onAuthorize}>
       Authorize YNAB
     </PrimaryButton>
+    <StrongText style={{ marginTop: 30 }}>Privacy Policy</StrongText>
+    <SecondaryText>
+      This website does not store any information from you or your YNAB account
+      on it's server. All data retrieved from the YNAB API is stored only in
+      your browser's local storage and is never transmitted to any other location or
+      third-party.
+    </SecondaryText>
   </Section>
 );
 
