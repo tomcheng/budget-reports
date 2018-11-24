aa93f2a2dd6899396f589de85f8017dcd86456d2
Thomas Cheng <thomascheng81@gmail.com>
2018-11-24 18:13:27 -0500

    Add Payees breakdown to income trends

--- src/components/Income.js
+++ src/components/Income.js
@@ -1,18 +1,29 @@
-import React, { PureComponent } from "react";
+import React, { Fragment, PureComponent } from "react";
 import PropTypes from "prop-types";
 import MonthByMonthSection from "./MonthByMonthSection";
+import { getTransactionMonth, sanitizeName } from "../budgetUtils";
+import moment from "moment";
+import GenericEntitiesSection from "./GenericEntitiesSection";
 
 class Income extends PureComponent {
   static propTypes = {
+    budget: PropTypes.shape({
+      payeesById: PropTypes.object.isRequired
+    }).isRequired,
     excludeFirstMonth: PropTypes.bool.isRequired,
     excludeLastMonth: PropTypes.bool.isRequired,
     onSetExclusion: PropTypes.func.isRequired,
     months: PropTypes.array.isRequired,
-    transactions: PropTypes.array.isRequired
+    transactions: PropTypes.arrayOf(
+      PropTypes.shape({
+        payee_id: PropTypes.string.isRequired
+      })
+    ).isRequired
   };
 
   state = {
-    selectedMonth: null
+    selectedMonth: null,
+    selectedPayeeId: null
   };
 
   handleSelectMonth = month => {
@@ -21,27 +32,70 @@ class Income extends PureComponent {
     }));
   };
 
+  handleSelectPayee = payeeId => {
+    this.setState(state => ({
+      selectedPayeeId: payeeId === state.selectedPayeeId ? null : payeeId
+    }));
+  };
+
   render() {
     const {
+      budget,
       excludeFirstMonth,
       excludeLastMonth,
       months,
       transactions,
       onSetExclusion
     } = this.props;
-    const { selectedMonth } = this.state;
+    const { selectedMonth, selectedPayeeId } = this.state;
+    const { payeesById } = budget;
+
+    const transactionsInSelectedMonth =
+      selectedMonth &&
+      transactions.filter(
+        transaction => getTransactionMonth(transaction) === selectedMonth
+      );
 
     return (
-      <MonthByMonthSection
-        excludeFirstMonth={excludeFirstMonth}
-        excludeLastMonth={excludeLastMonth}
-        months={months}
-        selectedMonth={selectedMonth}
-        title="Month by Month"
-        transactions={transactions}
-        onSelectMonth={this.handleSelectMonth}
-        onSetExclusion={onSetExclusion}
-      />
+      <Fragment>
+        <MonthByMonthSection
+          excludeFirstMonth={excludeFirstMonth}
+          excludeLastMonth={excludeLastMonth}
+          highlightFunction={
+            selectedPayeeId &&
+            (transaction => transaction.payee_id === selectedPayeeId)
+          }
+          months={months}
+          selectedMonth={selectedMonth}
+          title={
+            selectedPayeeId
+              ? `Month by Month: ${sanitizeName(
+                  payeesById[selectedPayeeId].name
+                )}`
+              : "Month by Month"
+          }
+          transactions={transactions}
+          onSelectMonth={this.handleSelectMonth}
+          onSetExclusion={onSetExclusion}
+        />
+        <GenericEntitiesSection
+          key={`payee-${selectedMonth || "all"}`}
+          entityKey="payee_id"
+          entitiesById={payeesById}
+          title={
+            selectedMonth
+              ? `Payees: ${moment(selectedMonth).format("MMMM")}`
+              : "Payees"
+          }
+          transactions={transactionsInSelectedMonth || transactions}
+          selectedEntityId={selectedPayeeId}
+          onClickEntity={this.handleSelectPayee}
+          showAverageToggle={false}
+          showAverage={!selectedMonth}
+          numMonths={months.length}
+          limitShowing
+        />
+      </Fragment>
     );
   }
 }
