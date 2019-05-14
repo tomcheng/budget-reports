f5466a2d4f4ec7a43cd758ce9455cc207dc291e3
Thomas Cheng <thomas.cheng@loopio.com>
2019-05-14 00:47:53 -0400

    Updates

---
+++ precache-manifest.3dabaf402c43a8eb6953e599e12300c8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0ed4839e8d7bcfa2a3afcce849061f",
+    "url": "/budget-reports/index.html"
+  },
+  {
+    "revision": "046c026eaa2dcb507f8d",
+    "url": "/budget-reports/static/js/2.0d057ee7.chunk.js"
+  },
+  {
+    "revision": "8662d6549115c91f24da",
+    "url": "/budget-reports/static/js/main.ad8b7009.chunk.js"
+  },
+  {
+    "revision": "ce0a2cbbcf8468c37184",
+    "url": "/budget-reports/static/js/runtime~main.eda4e144.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/budget-reports/precache-manifest.7a9749042acca535e8e71102a825c507.js"
+  "/budget-reports/precache-manifest.3dabaf402c43a8eb6953e599e12300c8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/budget-reports/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/budget-reports/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
