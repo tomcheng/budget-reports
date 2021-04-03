d8b97bb1ca09013bc50f7e081f51e23670eb0ed1
Thomas Cheng <thomas.cheng@shopify.com>
2021-04-03 11:54:53 -0400

    Add point of game

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ if (token) {
   initializeYnabApi(token);
 }
 
+/***
+ * You play the game to get points, but the point of the game is not
+ * the points, it's the joy you get getting those points. Whether you
+ * end up with a lot of points or very few points, you get the most
+ * value by having played the game to the best of your ability and
+ * getting maximal enjoyment from your time playing.
+ ***/
+
 ReactDOM.render(
   <HashRouter>
     <App hasToken={!!token} />
