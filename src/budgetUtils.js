a23edfe0f6f98d3d22e145ae303c1d14bb5b9c0e
Thomas Cheng <thomas.cheng@shopify.com>
2021-06-05 16:00:13 -0400

    Code cleanup

--- src/budgetUtils.js
+++ src/budgetUtils.js
@@ -1,45 +1,46 @@
 import compose from "lodash/fp/compose";
+import find from "lodash/fp/find";
 import pick from "lodash/fp/pick";
 import mapRaw from "lodash/fp/map";
-import get from "lodash/fp/get";
 import last from "lodash/fp/last";
 import { groupByProp, simpleMemoize, sumByProp } from "./dataUtils";
 
 const map = mapRaw.convert({ cap: false });
 
-export const isStartingBalanceOrReconciliation = simpleMemoize(budget => {
-  const startingBalancePayeeId = get("id")(
-    budget.payees.find(payee => payee.name === "Starting Balance")
+export const isStartingBalanceOrReconciliation = simpleMemoize((budget) => {
+  const startingBalancePayee = find({ name: "Starting Balance" })(
+    budget.payees
   );
-  const reconciliationPayeeId = get("id")(
-    budget.payees.find(
-      payee => payee.name === "Reconciliation Balance Adjustment"
-    )
-  );
+  const reconciliationPayee = find({
+    name: "Reconciliation Balance Adjustment",
+  })(budget.payees);
 
-  return transaction =>
-    transaction.payee_id === startingBalancePayeeId ||
-    transaction.payee_id === reconciliationPayeeId;
+  return (transaction) =>
+    transaction.payee_id === startingBalancePayee?.id ||
+    transaction.payee_id === reconciliationPayee?.id;
 });
 
-export const isIncome = simpleMemoize(budget => {
-  const toBeBudgetedId = get("id")(
-    budget.categories.find(category => category.name.match(/^Inflow: /))
+export const isIncome = simpleMemoize((budget) => {
+  const toBeBudgeted = find((category) => category.name.match(/^Inflow: /))(
+    budget.categories
   );
-  return transaction => transaction.category_id === toBeBudgetedId;
+  return (transaction) => transaction.category_id === toBeBudgeted?.id;
 });
 
-export const isTransfer = (investmentAccounts = {}) => transaction =>
-  !transaction.category_id ||
-  !!investmentAccounts[transaction.transfer_account_id];
+export const isTransfer =
+  (investmentAccounts = {}) =>
+  (transaction) =>
+    !transaction.category_id ||
+    Boolean(investmentAccounts[transaction.transfer_account_id]);
 
-export const getTransactionMonth = transaction => transaction.date.slice(0, 7);
+export const getTransactionMonth = (transaction) =>
+  transaction.date.slice(0, 7);
 
-const rejectNonCategoryTransations = simpleMemoize(transactions =>
-  transactions.filter(tran => !!tran.category_id)
+const rejectNonCategoryTransations = simpleMemoize((transactions) =>
+  transactions.filter((tran) => tran.category_id)
 );
 
-export const getFirstMonth = budget => {
+export const getFirstMonth = (budget) => {
   const filteredTransactions = rejectNonCategoryTransations(
     budget.transactions
   );
@@ -55,9 +56,9 @@ export const getPayeeNodes = ({ payeesById, transactions }, divideBy = 1) =>
       ...(payeesById[payeeId]
         ? pick(["id", "name"])(payeesById[payeeId])
         : { id: "no-payee", name: "(no payee)" }),
-      amount: sumByProp("amount")(transactions) / divideBy
+      amount: sumByProp("amount")(transactions) / divideBy,
     })),
-    groupByProp("payee_id")
+    groupByProp("payee_id"),
   ])(transactions);
 
-export const sanitizeName = name => name.replace(/[^a-zA-Z0-9 ]/g, "").trim();
+export const sanitizeName = (name) => name.replace(/[^a-zA-Z0-9 ]/g, "").trim();
